f914a59788ace14d71a6f245cd9f6190e3469d96
Théo LE GOFF <legoff.theo2@gmail.com>
2017-01-09 17:54:44 +0100

    Revert "Revert "Version 1.0""
    This reverts commit 3bcc693849b54b95d18b2657575f426d8dde8d55.

---
+++ Assets/Scripts/GetTime.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GetTime : MonoBehaviour {
+	void Update () {
+		GetComponent<Text> ().text = System.DateTime.Now.Hour + " : " + System.DateTime.Now.Minute;
+	}
+}
